4242cc66f6b19b155772e1619d44bee8846075e4
nagool <quietwave00@gmail.com>
2026-05-08 09:04:48 +0900

    캐시 설정

---
+++ src/main/java/org/example/owoonwan/config/StaticResourceCacheConfig.java
@@ -0,0 +1,23 @@
+package org.example.owoonwan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class StaticResourceCacheConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("classpath:/static/index.html")
+                .setCacheControl(CacheControl.noStore().mustRevalidate());
+
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic().immutable());
+    }
+}
